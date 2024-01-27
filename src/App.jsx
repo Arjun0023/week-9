8a1e4b21d391b2a44835d208c0c9f5f2ba5d48b7
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-01-27 19:31:04 +0530

    component mounted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { render } from 'react-dom'
 
 function App() {
+  console[render , setRender] = useState(true);
+  useEffect(() =>{
+    setTimeout(()=>{
+      setRender(false);
+    })
+  })
 
   return (
-      <MyComponent/>
+  <MyComponent/>
   )
 }
 function MyComponent() {
